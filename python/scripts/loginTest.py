31fb03758b68cbea762ba899c32445e0aac5e71d
sirkellsworth <>
2021-09-18 21:50:47 -0600

    removed password from script

--- python/scripts/loginTest.py
+++ python/scripts/loginTest.py
@@ -1,9 +1,11 @@
 import sys
 import os
 sys.path.append(os.path.join(sys.path[0], '../'))
+import Profile
 import RunescapeWindow
 
+profile = Profile.Profile("config/miner.py")
 window = RunescapeWindow.RunescapeWindow()
 
 window.worldPick()
-window.login("chadsbutt@gmail.com", "McDemShoulders")
+window.login(profile.emailGet(), profile.passwordGet())
